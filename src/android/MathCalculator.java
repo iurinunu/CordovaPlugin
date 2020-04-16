713e12e8d9355a805a8269fb3d9b7f87513c54bb
Iurie Nunu <iurie.nunu@crimsontide.co.uk>
2020-04-16 15:35:46 +0100

    java fixes

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -40,8 +40,8 @@ public class MathCalculator extends CordovaPlugin {
                 Intent intent = new Intent(Intent.ACTION_SENDTO);
                 intent.setType("*/*");
                 intent.putExtra(Intent.EXTRA_SUBJECT, "Hi, this is a task sent from my to-do app: " + p1);
-                if (callback.intent.resolveActivity(getPackageManager()) != null) {
-                    callback.startActivity(intent);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
                 }
 
                 callback.success("Email sent!");
